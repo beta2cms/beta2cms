97a5c98f1c05aec120bb07e287fb4cdef2703185
Philipp Wahle <phil.wahle@student.htw-berlin.de>
2015-10-10 21:01:25 +0200

    Page Controller extends

--- app/Http/Controllers/Backend/PageController.php
+++ app/Http/Controllers/Backend/PageController.php
@@ -22,9 +22,9 @@ class PageController extends Controller
 
     public function index()
     {
-        $pages = \App\Page::findBySlug('main')->children;
+        $main = \App\Page::findBySlug('main');
 
-        return view('admin.page.index', compact('pages'));
+        return view('admin.page.index', compact('main'));
     }
 
     /**
@@ -32,9 +32,10 @@ class PageController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        return view('admin.page.create');
+        $parent = $request->get('parent');
+        return view('admin.page.create', compact('parent'));
     }
 
     /**
@@ -46,18 +47,33 @@ class PageController extends Controller
     public function store(PageRequest $request)
     {
         $data = $request->all();
+
+        // todo: move to helper method
+        $data['active'] = !empty($data['active']);
+
+        // check if slug is empty
         if(empty($data['slug']))
         {
+            // create slug from name
             $data['slug'] = str_slug( $data['name'] );
         }
 
-        $exist =  \App\Page::findBySlug($data['slug'])->toArray();
+        // todo: create slug helper
+        $exist =  \App\Page::findBySlug($data['slug'])->get()->toArray();
         if(!empty($exist))
         {
             //$appendix = '-1';
             dd($data,'change slug');
-
         }
+
+        \App\Page::create($data)->save();
+        // todo: include flash
+//        flash()->success('New page created!');
+
+        // todo: insert after logic
+
+
+        return redirect()->route('admin.page.index');
     }
 
     /**
@@ -103,5 +119,21 @@ class PageController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    /**
+     * Toggle active flag on resource
+     * TODO: change to Ajax request
+     *
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function toggleActive($id)
+    {
+        $page = \App\Page::find($id);
+        $page->active = !boolval($page->active);
+        $page->save();
+
+        return redirect()->back();
     }
 }
