fc890dd3123e05c5044e67a22a3f817274bcd362
Philipp Wahle <phil.wahle@student.htw-berlin.de>
2015-10-12 17:49:02 +0200

    Add Active column to modules table

---
+++ database/migrations/2015_10_12_135654_add_active_to_modules_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddActiveToModulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('modules', function (Blueprint $table) {
+            $table->boolean('active')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('modules', function (Blueprint $table) {
+            $table->dropColumn('active');
+        });
+    }
+}
