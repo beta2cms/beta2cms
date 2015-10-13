a231825d1edd244ab2908dd21f3836a35107c641
Philipp Wahle <phil.wahle@student.htw-berlin.de>
2015-10-13 17:02:11 +0200

    extends element preview index

--- resources/views/admin/element/index.blade.php
+++ resources/views/admin/element/index.blade.php
@@ -1,9 +1,9 @@
-@extends('admin.master')
+{{--@inject('modules','App\Providers\ModuleServiceProvider' )--}}
 
+@extends('admin.master')
 
 @section('title', 'Site Elements')
 @section('subheading', $node->name)
-
 
 @section('breadcrumbs')
     {!! Breadcrumbs::render('element') !!}
@@ -27,9 +27,10 @@
             @foreach($node->content as $key=>$item)
                 <tr class="{{ !$item->active ? 'text-muted' : ''}}">
                     <th scope="row">{{ ++$key}}</th>
-                    <td colspan="2" >{{ $item->element->module->name }}</td>
+                    <td colspan="2">{{ $item->element->module->name }}</td>
                     {{--<td>{{$node->deleted_at}}</td>--}}
                     <td>
+                        todo: admin modules
                         {{--<a href="{{route('admin.module.undo', $node->id)}}" class="btn btn-xs btn-info"><i class="fa fa-refresh"></i></a>&nbsp;--}}
                         {{--<a href=""><i class="fa fa-trash"></i></a>--}}
                         {{--@include('admin.partials.toggle',[--}}
@@ -38,6 +39,16 @@
                         {{--])--}}
                     </td>
                 </tr>
+            <tr class="{{ !$item->active ? 'text-muted' : ''}}">
+                <th></th>
+                <td colspan="3">
+                    @if($item->element->module->active)
+                        {!!  $render->preview($item->element->module_id, $item->element->row)  !!}
+                    @else
+                        <b>No preview Specified.</b>
+                    @endif
+                </td>
+            </tr>
         @endforeach
     </div>
 
