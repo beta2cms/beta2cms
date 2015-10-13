cd39ba3a0c65ee514bf0b2f592270abb0c49fdcd
Philipp Wahle <phil.wahle@student.htw-berlin.de>
2015-10-13 16:59:56 +0200

    unfinish: Add more functionality to ModuleServiceProvider

--- app/Providers/ModuleServiceProvider.php
+++ app/Providers/ModuleServiceProvider.php
@@ -2,12 +2,24 @@
 
 namespace App\Providers;
 
+use App\Module;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 
 class ModuleServiceProvider extends ServiceProvider
 {
+
+
+    /**
+     * The Current used module
+     *
+     * @var null
+     */
+    private $module = null;
+
+    private $element  = null;
+
     /**
      * Bootstrap the application services.
      *
@@ -40,20 +52,28 @@ class ModuleServiceProvider extends ServiceProvider
     public function preview($module_id, $element_id)
     {
 
-        $module = \App\Module::findOrFail($module_id);
+        $this->isModule($module_id);
 
-        return App::make('module:' . strtolower($module->name))->preview($element_id);
+        return App::make('module:' . strtolower($this->module->name))->preview($element_id);
     }
 
 
     public function create($module_id)
     {
+        $this->isModule($module_id);
 
+        return App::make('module:' . strtolower($this->module->name))->create();
     }
 
-    public function store($module_id, Request $request)
+    public function store($request)
     {
+        $data = $request->all();
 
+        $this->isModule($data['module']);
+
+        dd($data);
+        $store =  $this->element->create($request->all());
+        dd($store);
     }
 
     public function edit($module_id)
@@ -68,6 +88,22 @@ class ModuleServiceProvider extends ServiceProvider
 
     public function destroy($module_id)
     {
+
+    }
+
+    /**
+     * Check if the used Module is the current one
+     *
+     * @param $module_id
+     */
+    private function isModule($module_id)
+    {
+        if(!isset($this->module) || $this->module->id !== $module_id)
+        {
+            $this->module = Module::findOrFail($module_id);
+            $this->element = App::make('module:' . strtolower($this->module->name));
+        }
+
 
     }
 }
