5f95ce93f8a59946f6d7f7e281692a8ad1d59b80
Philipp Wahle <phil.wahle@student.htw-berlin.de>
2015-10-15 22:49:26 +0200

    Add Data Helper trait

---
+++ app/Helpers/Data.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Helpers;
+
+trait Data {
+
+    /**
+     * Check if the active field is set
+     * If it's set => true,
+     * If not => false
+     *
+     * @param $data
+     * @return mixed
+     */
+    public static function checkActive($data)
+    {
+        $data['active'] = isset($data['active']);
+        return $data;
+    }
+
+
+    /**
+     * Rename the Field of an array and forget the old one
+     *
+     * @param $data
+     * @param $from
+     * @param $to
+     * @return mixed
+     */
+    public static function renameField($data, $from, $to)
+    {
+        $data[$to] = $data[$from];
+        array_forget($data, $from);
+        return $data;
+    }
+
+    /**
+     * Add a key-Value-Pair to a given array
+     *
+     * @param $data
+     * @param $key
+     * @param $value
+     * @return array
+     */
+    public static function addField($data, $key, $value)
+    {
+           return array_add($data, $key, $value);
+    }
+
+}
